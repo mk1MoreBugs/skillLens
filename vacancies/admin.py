9c34b5c732cce83c844dd5c4161d226abe7de9bd
mk1MoreBugs <oleg1202000@gmail.com>
2026-09-05 11:29:12 +0300

    create models Vacancy and Skill

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vacancy, Skill
+
+admin.site.register([Vacancy, Skill])
--- vacancies/models.py
+++ vacancies/models.py
@@ -1,3 +1,43 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=True
+    )
+    date_add = models.DateTimeField(
+        auto_now_add=True,
+    )
+    name = models.CharField(max_length=500)
+
+    class Meta:
+        db_table = 'skills'
+
+
+class Vacancy(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=True
+    )
+    date_add = models.DateTimeField(
+        auto_now_add=True,
+    )
+    date_published = models.DateTimeField()
+    name = models.CharField(max_length=500)
+    description = models.TextField(null=True)
+    URL = models.URLField()
+
+    skills = models.ManyToManyField(
+        Skill,
+        related_name='vacancies',
+        db_table='vacancies_skills_map',
+    )
+
+    class Meta:
+        db_table = 'vacancies'
+
