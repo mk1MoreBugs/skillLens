cb82048dc60c9a0b06f9cda6eba2cc1b58ded05a
mk1MoreBugs <oleg1202000@gmail.com>
2026-09-26 19:33:23 +0300

    Add base vacancies views

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -1,2 +1,8 @@
+from django.urls import path
 
-urlpatterns = []
+from . import views
+
+urlpatterns = [
+    path("index.html", views.vacancy_about_app, name="vacancy-about-app"),
+    path("list", views.vacancy_list, name="vacancy-list"),
+]
--- vacancies/views.py
+++ vacancies/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Vacancy
+
+def vacancy_about_app(request):
+    return HttpResponse("<h1>Vacancies app</h1>")
+
+def vacancy_list(request):
+    vacancies =Vacancy.objects.all().order_by("-date_published")
+    context = {"vacancies": vacancies}
+    return render(
+        request,
+        template_name="vacancies/list.html",
+        context=context,
+    )
